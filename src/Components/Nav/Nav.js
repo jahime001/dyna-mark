d78d62197112a18f3c2337b4f6deba69054b6029
Bibhor2000 <daxter740@gmail.com>
2022-12-08 18:37:42 -0500

    added styling to navbar component

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,14 +1,20 @@
 import { React, Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
+import Logo from '../../../assets/DynaLong.png'
 
 export default function Nav() {
   return (
     <div className='NavBar'>
-        <Link to='/About'>About</Link>
-        <Link to='/Discover'>Discover</Link>
-        <Link to='/Home'>Home</Link>
-        <Link to='/NewBookmark'>New Bookmark</Link>
+        <div className='NavLogo'>
+            <h1>Logo</h1>
+        </div>
+        <div className='NavLinks'>
+            <Link to='/Home'><a>Home</a></Link>
+            <Link to='/Discover'><a>Discover</a></Link>
+            <Link to='/NewBookmark'><a>New Bookmark</a></Link>
+            <Link to='/About'><a>About</a></Link>
+        </div>
     </div>
   )
 }
