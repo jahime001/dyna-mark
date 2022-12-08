1ad8988d4a4c2e74a18a84b84cafa2163eb3434c
mobrewer <morgiearan@gmail.com>
2022-12-08 16:53:43 -0700

    Declared states needed to store input values

--- src/Components/NewBookmark/NewBookmark.js
+++ src/Components/NewBookmark/NewBookmark.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
 
 export default function newBookmark() {
+  const [title, setTitle] = useState('')
+  const [preview, setPreview] = useState('')
+  const [description, setDescription] = useState('')
+  const [link, setLink] = useState('')
+  const [likes, serLikes] = useState(1)
+  const [dislikes, setDislikes] = useState(1)
+  const [message, setMessage] = useState('')
+  const navigate = useNavigate()
+
+
+
+
   return (
     <div>newBookmark</div>
   )
