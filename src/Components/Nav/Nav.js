32c5995e7c9c15e7b39f278ae1e96960749c7429
Bibhor2000 <daxter740@gmail.com>
2022-12-08 18:57:09 -0500

    Finished current styling for navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,13 @@
 import { React, Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
-import Logo from '../../assets/DynaLong.png'
+import Logo from '../../assets/DynaV2.png'
 
 export default function Nav() {
   return (
     <div className='NavBar'>
         <div className='NavLogo'>
-            <img src={Logo} alt='Logo'/>
+            <img src={Logo} alt='Logo' className='logo'/>
         </div>
         <div className='NavLinks'>
             <Link to='/Home'><a>Home</a></Link>
