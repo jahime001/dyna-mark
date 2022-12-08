1c58b117b5a7cb71b675717d841fe7bffd4182be
Bibhor2000 <daxter740@gmail.com>
2022-12-08 18:10:22 -0500

    created base structure for the nav component

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import { React, Component } from 'react'
+import { Link } from 'react-router'
+import styles from '../Nav/Nav.css'
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div className='NavBar'>
+        <Link to='/About'>About</Link>
+        <Link to='/Discover'>Discover</Link>
+        <Link to='/Home'>Home</Link>
+        <Link to='/NewBookmark'>New Bookmark</Link>
+    </div>
   )
 }
