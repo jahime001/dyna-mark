414a4ccb366cf8ef11f06e1ee6969e4905957201
Bibhor2000 <daxter740@gmail.com>
2022-12-09 12:25:31 -0500

    created base for discover component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Discover from './Components/Discover/Discover';
 import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Discover/>
     </div>
   );
 }
