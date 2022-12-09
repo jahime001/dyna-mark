991e231b81410f71cc9c5935272fb39904c0f2a0
mobrewer <morgiearan@gmail.com>
2022-12-09 08:48:41 -0700

    Went back to previous code, was able to create new resource!

--- src/Components/NewBookmark/NewBookmark.js
+++ src/Components/NewBookmark/NewBookmark.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 import { set } from 'mongoose'
-
+import { useNavigate } from 'react-router-dom'
 
 export default function NewBookmark() {
   const [title, setTitle] = useState('')
@@ -11,68 +11,68 @@ export default function NewBookmark() {
   const [likes, setLikes] = useState(1)
   const [dislikes, setDislikes] = useState(1)
   const [message, setMessage] = useState('')
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault()
-  //   console.log(JSON.stringify({
-  //     title: title,
-  //     preview: preview,
-  //     description: description,
-  //     link: link,
-  //     likes: likes,
-  //     dislikes: dislikes,
-  //   }));
-
-  //   try {
-  //     const data = {
-  //       title: title,
-  //       preview: preview,
-  //       description: description,
-  //       link: link,
-  //       likes: likes,
-  //       dislikes: dislikes,
-  //     }
-  //     console.log(data);
-  //     const response = await axios.post('https://dyna-mark.fly.dev/api/bookmark/', data)
-  //     console.log(response.data);
-  //     navigate(-1)
-  //   } catch(err) {
-  //     next(err)
-  //   }
-  // }
-
-
-  let handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    console.log(JSON.stringify({
+      title: title,
+      preview: preview,
+      description: description,
+      link: link,
+      likes: likes,
+      dislikes: dislikes,
+    }));
+
     try {
-      let res = await fetch('https://dyna-mark.fly.dev/api/bookmark/', {
-        method: 'POST',
-        body: JSON.stringify({
-          title: title,
-          preview: preview,
-          description: description,
-          link: link,
-          likes: likes,
-          dislikes: dislikes,
-        })
-      })
-      let resJson = await res.json()
-      if (res.status === 200) {
-        setTitle('')
-        setPreview('')
-        setDescription('')
-        setLink('')
-        setLikes(1)
-        setDislikes(1)
-        setMessage('New resource was created successfully!')
-      } else {
-        setMessage('Some error occured')
+      const data = {
+        title: title,
+        preview: preview,
+        description: description,
+        link: link,
+        likes: likes,
+        dislikes: dislikes,
       }
+      console.log(data);
+      const response = await axios.post('https://dyna-mark.fly.dev/api/bookmark/', data)
+      console.log(response.data);
+      navigate(-1)
     } catch(err) {
-      // next(err)
+      console.log(err);
     }
   }
+
+
+  // let handleSubmit = async(e) => {
+  //   e.preventDefault()
+  //   try {
+  //     let res = await fetch('https://dyna-mark.fly.dev/api/bookmark/', {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         title: title,
+  //         preview: preview,
+  //         description: description,
+  //         link: link,
+  //         likes: likes,
+  //         dislikes: dislikes,
+  //       })
+  //     })
+  //     let resJson = await res.json()
+  //     if (res.status === 200) {
+  //       setTitle('')
+  //       setPreview('')
+  //       setDescription('')
+  //       setLink('')
+  //       setLikes(1)
+  //       setDislikes(1)
+  //       setMessage('New resource was created successfully!')
+  //     } else {
+  //       setMessage('Some error occured')
+  //     }
+  //   } catch(err) {
+  //     console.log(err);
+  //   }
+  // }
 
 
   return (
