bdc7563c2632d64b265cd1c6db008f1d285130ba
mobrewer <morgiearan@gmail.com>
2022-12-09 09:28:29 -0700

    Added h2, re-arranged code to get setMessage to work

--- src/Components/NewBookmark/NewBookmark.js
+++ src/Components/NewBookmark/NewBookmark.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import axios from 'axios'
-import { set } from 'mongoose'
 import { useNavigate } from 'react-router-dom'
 
 export default function NewBookmark() {
@@ -8,8 +7,7 @@ export default function NewBookmark() {
   const [preview, setPreview] = useState('')
   const [description, setDescription] = useState('')
   const [link, setLink] = useState('')
-  const [likes, setLikes] = useState(1)
-  const [dislikes, setDislikes] = useState(1)
+  const [tags, setTags] =useState([])
   const [message, setMessage] = useState('')
   const navigate = useNavigate()
 
@@ -20,8 +18,7 @@ export default function NewBookmark() {
       preview: preview,
       description: description,
       link: link,
-      likes: likes,
-      dislikes: dislikes,
+      tags: tags
     }));
 
     try {
@@ -30,53 +27,26 @@ export default function NewBookmark() {
         preview: preview,
         description: description,
         link: link,
-        likes: likes,
-        dislikes: dislikes,
+        tags: tags
       }
-      console.log(data);
-      const response = await axios.post('https://dyna-mark.fly.dev/api/bookmark/', data)
-      console.log(response.data);
-      navigate(-1)
-    } catch(err) {
-      console.log(err);
-    }
-  }
-
-
-  // let handleSubmit = async(e) => {
-  //   e.preventDefault()
-  //   try {
-  //     let res = await fetch('https://dyna-mark.fly.dev/api/bookmark/', {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         title: title,
-  //         preview: preview,
-  //         description: description,
-  //         link: link,
-  //         likes: likes,
-  //         dislikes: dislikes,
-  //       })
-  //     })
-  //     let resJson = await res.json()
-  //     if (res.status === 200) {
-  //       setTitle('')
-  //       setPreview('')
-  //       setDescription('')
-  //       setLink('')
-  //       setLikes(1)
-  //       setDislikes(1)
-  //       setMessage('New resource was created successfully!')
-  //     } else {
-  //       setMessage('Some error occured')
-  //     }
-  //   } catch(err) {
-  //     console.log(err);
-  //   }
-  // }
-
+    const res = await axios.post('https://dyna-mark.fly.dev/api/bookmark/', data)
+    .then(function (response) {
+      console.log(response)
+      console.log('Resource was created succesfully');
+      setMessage('Resource was created succesfully')
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log('Some error occured')
+      setMessage('Some error occured')
+    });
+    navigate(-1)
+  } catch(err) {}
+}
 
   return (
-    <div className='Bookmark'>
+    <div>
+      <h2>Add your favorite resource here:</h2>
       <form onSubmit={handleSubmit}>
         <input
           type='text'
@@ -105,6 +75,13 @@ export default function NewBookmark() {
           placeholder='Enter website link'
           onChange={(e) => setLink(e.target.value)}
         />
+        <br />
+        <input
+          type='tags'
+          value={tags}
+          placeholder='Enter tags'
+          onChange={(e) => setTags(e.target.value)}
+        ></input>
         <br />
         <button type='submit'>Create</button>
         <div className='message'>{message ? <p>{message}</p> : null }</div>
