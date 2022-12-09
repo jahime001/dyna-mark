8fe51c82dea821c0bf5f7e98da0a9db7f97a0545
mobrewer <morgiearan@gmail.com>
2022-12-09 07:50:30 -0700

    Trying a different function including if else statement

--- src/Components/NewBookmark/NewBookmark.js
+++ src/Components/NewBookmark/NewBookmark.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { set } from 'mongoose'
 
 
 export default function newBookmark() {
@@ -7,27 +8,69 @@ export default function newBookmark() {
   const [preview, setPreview] = useState('')
   const [description, setDescription] = useState('')
   const [link, setLink] = useState('')
-  const [likes, serLikes] = useState(1)
+  const [likes, setLikes] = useState(1)
   const [dislikes, setDislikes] = useState(1)
   const [message, setMessage] = useState('')
   const navigate = useNavigate()
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    console.log(JSON.stringify({
-      title: title,
-      preview: preview,
-      description: description,
-      link: link,
-      likes: likes,
-      dislikes: dislikes,
-    }));
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   console.log(JSON.stringify({
+  //     title: title,
+  //     preview: preview,
+  //     description: description,
+  //     link: link,
+  //     likes: likes,
+  //     dislikes: dislikes,
+  //   }));
 
+  //   try {
+  //     const data = {
+  //       title: title,
+  //       preview: preview,
+  //       description: description,
+  //       link: link,
+  //       likes: likes,
+  //       dislikes: dislikes,
+  //     }
+  //     console.log(data);
+  //     const response = await axios.post('https://dyna-mark.fly.dev/api/bookmark/', data)
+  //     console.log(response.data);
+  //     navigate(-1)
+  //   } catch(err) {
+  //     next(err)
+  //   }
+  // }
+
+
+  let handleSubmit = async(e) => {
+    e.preventDefault()
     try {
-      const data = {
-        title: title,
-        
+      let res = await fetch('https://dyna-mark.fly.dev/api/bookmark/', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: title,
+          preview: preview,
+          description: description,
+          link: link,
+          likes: likes,
+          dislikes: dislikes,
+        })
+      })
+      let resJson = await res.json()
+      if (res.status === 200) {
+        setTitle('')
+        setPreview('')
+        setDescription('')
+        setLink('')
+        setLikes(1)
+        setDislikes(1)
+        setMessage('New resource was created successfully!')
+      } else {
+        setMessage('Some error occured')
       }
+    } catch(err) {
+      next(err)
     }
   }
 
