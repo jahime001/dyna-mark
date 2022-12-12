f762dc68ed28c42c0a316d909d647af2f2cc13b6
Bibhor2000 <daxter740@gmail.com>
2022-12-12 16:17:54 -0500

    added a function for switching themes between dark & light using ternary operators as well as added a button with an onclick function in the return statement of app js

--- src/App.js
+++ src/App.js
@@ -35,9 +35,17 @@ function App() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+  }
+
   return (
-    <div>
+    <div className='app' data-theme={theme}>
       <Nav/>
+      <button onClick={switchTheme}>
+        Switch to {theme === 'light' ? 'Dark' : "Light"} Theme
+      </button>
   
       <main>
         <Routes>
