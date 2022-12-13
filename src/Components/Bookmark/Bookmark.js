06141ca6d4ef81ba70bb52d326a9f884e7fd5f4a
jahime001 <jahimecamea@gmail.com>
2022-12-12 19:46:40 -0500

    bookmark

--- src/Components/Bookmark/Bookmark.js
+++ src/Components/Bookmark/Bookmark.js
@@ -4,6 +4,8 @@ import { useParams } from 'react-router'
 import axios from 'axios'
 import { FiThumbsUp } from 'react-icons/fi';
 import { FiThumbsDown } from 'react-icons/fi';
+import { MdDelete } from 'react-icons/md';
+import { RiExternalLinkFill } from 'react-icons/ri';
 
 export default function Bookmark() {
   let {id} = useParams()
@@ -47,20 +49,46 @@ const [booke, setBooke] = useState([])
     setChanged(data.likes)
     console.log(booke)
   }
-  function handleDislike(){
+  function handleDislike() {
+    const data = {
+      ...booke,
+      dislikes: booke.dislikes + 1
+    }
+    axios.put(`https://dyna-mark.fly.dev/api/bookmark/${id}`, data, {
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
+    // setBooke(data)
+    chosenBookmark()
+    setChanged(data.dislikes)
+    console.log(booke)
   }
+
+
+ 
 
   if (booke){
     return (
       <div className='bookmark'>
-            <h1>{booke.title}</h1>
-            <h1>{booke.description}</h1>
-            <h1>{booke.link}</h1>
-            <FiThumbsUp onClick={handleLike} changed={changed}/>
-            <h1>{booke.likes}</h1>
-            <FiThumbsDown onClick={handleDislike}/>
-            <h1>{booke.dislikes}</h1>
+        <div className='booke'>
+          <MdDelete />
+          <h1>{booke.title}</h1>
+          <h1>{booke.description}</h1>
+          <a href={booke.link} target='blank'><div className='booke-link'>Visit   <RiExternalLinkFill/></div></a>
+          <FiThumbsUp onClick={handleLike} changed={changed} />
+          <h1>{booke.likes}</h1>
+          <FiThumbsDown onClick={handleDislike} />
+          <h1>{booke.dislikes}</h1>
+
+        </div>
       </div>
     )
   } else{
