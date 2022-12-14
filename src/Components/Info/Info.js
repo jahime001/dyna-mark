649ff367a8a48fccad55eeb874f527e90850f604
jahime001 <jahimecamea@gmail.com>
2022-12-13 19:49:08 -0500

    discover page query

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -3,6 +3,7 @@ import './Info.css'
 import bx1 from '../../assets/Box1.png'
 import bx2 from '../../assets/Box2.png'
 import bx3 from '../../assets/Box3.png'
+import { Link } from 'react-router-dom'
 
 export default function Info() {
    const [myclass, setmyClass] = useState('hide')
@@ -26,18 +27,14 @@ window.addEventListener("scroll", handleShow);
                 </div>
                 <h1>What Is Dyna-Mark</h1>
                 <p>Dyna-Mark takes saving bookmarks to a whole new level. The ability to find exactly what you need easily through search or categories makes Dyna-Mark more efficient then any bookmark system in your browser!</p>
-                
+              <Link to={"/discover"}><div className='box-button'>See More</div></Link>  
               </div>
               <div className={`${myclass}`} style={{ transition: "all 1.5s" }}>
                   <div className='art' style={{ backgroundImage: `url(${bx2})`, backgroundPosition: "center" }}>
                  </div>
                   <h1>Share Your Favorites!</h1>
-                  <p>Share resources/websites that helped you while coding to </p>
-              </div>
-              <div className={`${myclass}`} style={{ transition: "all 2s" }}>
-                  <div className='art' style={{ backgroundImage: `url(${bx3})`, backgroundPosition: "center" }}>
-                    
-                </div>
+                  <p>Share resources/websites that helped you while coding. Add tags </p>
+                  <Link to={"/newbookmark"}><div className='box-button'>Create!</div></Link>  
               </div>
           </div>
       </div>
