c44d8e6a9503c9f28809f3391b20030f64304894
jahime001 <jahimecamea@gmail.com>
2022-12-13 19:04:03 -0500

    home page media query

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,28 +3,44 @@ import { Link } from 'react-router-dom'
 import './Nav.css'
 import Logo from '../../assets/DynaV2.png'
 import useLocalStorage from 'use-local-storage'
+import {RxHamburgerMenu} from 'react-icons/rx'
+import {GrClose} from 'react-icons/gr'
+import {BsSunFill} from 'react-icons/bs'
+import {BsMoonFill} from 'react-icons/bs'
 
 
 export default function Nav({theme, setTheme, defaultDark, switchTheme} ) {
 const [showNav, setShowNav] = useState(false)
-  const toggleNavItems = () => {
-  setShowNav(!showNav)
-}
+ const handleShowNavbar = () => {
+    setShowNav(!showNav)
+  }
 
   return (
+    <div className='navBar-container'>
+
+  
     <div className='NavBar'>
         <div className='NavLogo'>
             <img src={Logo} alt='Logo' className='logo'/>
         </div>
         <button className='theme-button' onClick={switchTheme}>
-        Switch to {theme === 'light' ? 'Dark' : "Light"} Theme
+        {theme === 'light' ? <BsSunFill /> : <BsMoonFill />}
         </button>
-        <div className='NavLinks'>
-            <Link to='/'>Home</Link>
-            <Link to='/discover'>Discover</Link>
-            <Link to='/newbookmark'>New bookmark</Link>
-            <Link to='/about'>About</Link>
+        
         </div>
+        <div className='nav-icons'>
+          {(showNav === false)?
+            <RxHamburgerMenu onClick={handleShowNavbar} className='nav-icon'/>:
+            <GrClose onClick={handleShowNavbar} className='nav-icon'/>
+          }
+        </div>
+        <div className={`NavLinks ${showNav && 'active'}`}>
+            <Link to='/' onClick={handleShowNavbar}>Home</Link>
+            <Link to='/discover' onClick={handleShowNavbar}>Discover</Link>
+            <Link to='/newbookmark' onClick={handleShowNavbar}>New bookmark</Link>
+            <Link to='/about' onClick={handleShowNavbar}>About</Link>
+        </div>
+    
     </div>
   )
 }
